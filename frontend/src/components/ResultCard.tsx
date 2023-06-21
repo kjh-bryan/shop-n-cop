03ed4bf4acbed4690ba058a34b6fa066a12e9f94
DESKTOP-QQRB065\shiri <shirinruth6@gmail.com>
2023-06-22 01:01:22 +0800

    did the front end for results page - wip

---
+++ frontend/src/components/ResultCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Image, ImageSourcePropType } from 'react-native';
+import { styles } from '../styles/resultsCard_styles';
+
+interface ResultsCardProps {
+  title: string;
+  content: string;
+  price: string;
+  image: ImageSourcePropType;
+}
+
+const ResultsCard: React.FC<ResultsCardProps> = ({ title, content, image, price }) => {
+
+  return (
+    <View style={styles.cardContainer}>
+        {/* for image url purpose */}
+       {/* <Image source={{ uri: image }} /> */}
+       <Image source={image} />
+      <Text style={styles.productTitle}>{title}</Text>
+      <Text style={styles.productShop}>{content}</Text>
+      <Text style={styles.productPrice}>{price}</Text>
+    </View>
+  );
+};
+
+export default ResultsCard;
