7e234a03ed58f759fa003fe1213d791fbe46dfc0
Bryan Kang <kjhbryan@gmail.com>
2023-06-22 22:49:49 +0800

    added light styles for styled text

--- frontend/src/components/StyledText.tsx
+++ frontend/src/components/StyledText.tsx
@@ -6,26 +6,40 @@ import { StyleSheet, StyleSheetProperties, Text, TextProps } from 'react-native'
 
 
 
-export const StyledText = ({title, style,isBold} : {title:string, style?:any,isBold:boolean}) => {
+export const StyledText = ({title, style,isBold,isLight} : {title:string, style?:any,isBold:boolean,isLight:boolean}) => {
 
      let [fontsLoaded] = useFonts({
        "OpenSans-Regular": require("../../assets/fonts/OpenSans-Regular.ttf"),
        "OpenSans-Bold": require("../../assets/fonts/OpenSans-Bold.ttf"),
+       "OpenSans-Light": require("../../assets/fonts/OpenSans-Light.ttf"),
      });
 
      if (!fontsLoaded) {
        return <AppLoading />;
-     }
+      }
        return (
-         <Text style={[isBold ? styles.bold : styles.regular, style]}>
-           {title}
-         </Text>
+         <>
+           {isBold && !isLight && (
+             <Text style={[styles.bold, style]}>{title}</Text>
+           )}
+
+           {!isBold && isLight && (
+             <Text style={[styles.light, style]}>{title}</Text>
+           )}
+
+           {!isBold && !isLight && (
+             <Text style={[styles.regular, style]}>{title}</Text>
+           )}
+         </>
        );
 };
 
 const styles = StyleSheet.create({
   regular: {
     fontFamily: "OpenSans-Regular",
+  },
+  light: {
+    fontFamily: "OpenSans-Light",
   },
   bold: {
     fontFamily: "OpenSans-Bold",
