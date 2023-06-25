41a8797ad396308a34769df273de164760dc46cd
Bryan Kang <kjhbryan@gmail.com>
2023-06-25 21:17:05 +0800

    temporary fix to require cycle issue

--- frontend/src/screens/SignInScreen.tsx
+++ frontend/src/screens/SignInScreen.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/SignInScreen.style";
+import styles from '../styles/SignInScreen.style';
 import {
   SafeAreaView,
   TextInput,
@@ -10,56 +10,59 @@ import {
   Platform,
   Keyboard,
   TouchableWithoutFeedback,
-} from "react-native";
-import React, { useState } from "react";
-import { StyledText } from "../components";
-import { useNavigation } from "@react-navigation/native";
-import { ShopNCopStackNavigation, StackParams } from "../navigation";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+} from 'react-native';
+import React, { useState } from 'react';
+import { StyledText } from '../components';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackParams } from '../navigation/NavigationTypes';
+import { ShopNCopStackNavigation } from '../navigation/NavigationConstants';
 
 export const SignInScreen = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.container}>
         <KeyboardAvoidingView
           style={styles.topHalf}
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         >
-          <Image source={require("../../assets/logo/logo.png")} />
-          <StyledText 
-                 title="Sign In"
-                 style={styles.signInHeaderText}
-                 isBold={true}
-                 isLight={false}/>
+          <Image source={require('../../assets/logo/logo.png')} />
+          <StyledText
+            title='Sign In'
+            style={styles.signInHeaderText}
+            isBold={true}
+            isLight={false}
+          />
           <TextInput
             style={styles.emailAndPasswordTextBox}
             onChangeText={setEmail}
             value={email}
-            placeholder="Email"
+            placeholder='Email'
           />
           <TextInput
             secureTextEntry={true}
             style={styles.emailAndPasswordTextBox}
             onChangeText={setPassword}
             value={password}
-            placeholder="Password"
+            placeholder='Password'
           />
           <TouchableOpacity
             onPress={() => {
               navigation.replace(ShopNCopStackNavigation.search, {
-                userId: "test",
+                userId: 'test',
               });
             }}
           >
             <View style={styles.signInButtonContainer}>
               <StyledText
-                     title="Sign In"
-                     style={styles.signInButtonText}
-                     isBold={false}
-                     isLight={false}/>
+                title='Sign In'
+                style={styles.signInButtonText}
+                isBold={false}
+                isLight={false}
+              />
             </View>
           </TouchableOpacity>
         </KeyboardAvoidingView>
@@ -68,33 +71,33 @@ export const SignInScreen = () => {
 
           <View style={styles.signInWithAppsContainer}>
             <TouchableOpacity
-              onPress={() => console.log("apple button pressed")}
+              onPress={() => console.log('apple button pressed')}
             >
               <Image
-                source={require("../../assets/apple-login/apple-login.png")}
+                source={require('../../assets/apple-login/apple-login.png')}
                 style={styles.brandImage}
               />
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => console.log("google button pressed")}
+              onPress={() => console.log('google button pressed')}
             >
               <Image
-                source={require("../../assets/google-login/google-login.png")}
+                source={require('../../assets/google-login/google-login.png')}
                 style={styles.brandImage}
               />
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => console.log("facebook button pressed")}
+              onPress={() => console.log('facebook button pressed')}
             >
               <Image
-                source={require("../../assets/facebook-login/facebook-login.png")}
+                source={require('../../assets/facebook-login/facebook-login.png')}
                 style={styles.brandImage}
               />
             </TouchableOpacity>
           </View>
           <View style={styles.notMemberContainer}>
             <Text style={styles.notMemberText}>Not a member?</Text>
-        
+
             <TouchableOpacity
               onPress={() => {
                 navigation.navigate({
