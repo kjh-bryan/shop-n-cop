da1d5d7812b2b8aaa2ae10c59e19532e6213f999
Bryan Kang <kjhbryan@gmail.com>
2023-06-25 20:11:45 +0800

    loading font improvement, remove deprecated app loading, add missing package

--- frontend/src/utils/loadFont.ts
+++ frontend/src/utils/loadFont.ts
@@ -1,13 +1,12 @@
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 
-export const loadFonts = (): [boolean, Error | null] => {
-    const response = useFonts({
-        "OpenSans-Regular": require("../../assets/fonts/OpenSans/OpenSans-Regular.ttf"),
-        "OpenSans-Bold": require("../../assets/fonts/OpenSans/OpenSans-Bold.ttf"),
-        "OpenSans-Light": require("../../assets/fonts/OpenSans/OpenSans-Light.ttf"),
-        "Roboto-Bold": require("../../assets/fonts/Roboto/Roboto-Bold.ttf"),
-        "Roboto-Medium": require("../../assets/fonts/Roboto/Roboto-Medium.ttf"),
-        "Roboto-Light": require("../../assets/fonts/Roboto/Roboto-Light.ttf"),
-      }); 
-      return response;
-}
+export const loadFonts = async () => {
+  await Font.loadAsync({
+    'OpenSans-Regular': require('../../assets/fonts/OpenSans/OpenSans-Regular.ttf'),
+    'OpenSans-Bold': require('../../assets/fonts/OpenSans/OpenSans-Bold.ttf'),
+    'OpenSans-Light': require('../../assets/fonts/OpenSans/OpenSans-Light.ttf'),
+    'Roboto-Bold': require('../../assets/fonts/Roboto/Roboto-Bold.ttf'),
+    'Roboto-Medium': require('../../assets/fonts/Roboto/Roboto-Medium.ttf'),
+    'Roboto-Light': require('../../assets/fonts/Roboto/Roboto-Light.ttf'),
+  });
+};
