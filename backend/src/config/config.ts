920a76f08c26ebfa3f9833f91f54a292184fa26e
Bryan Kang <bryan.kang@activate.sg>
2023-06-29 11:45:48 +0800

    implemented post links to mongo do, seach text with serpapi api

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -9,5 +9,7 @@ const SERVER_PORT = process.env.SERVER_PORT
 export const config = {
   server: {
     port: SERVER_PORT,
+    apiVersion: process.env.APIVERSION,
+    serpapiApiKey: process.env.SERPAPI_API_KEY,
   },
 };
