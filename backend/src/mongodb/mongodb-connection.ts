f0c939f0f71f4265953a7d21747bd4c9fb91e126
GERMAINE <>
2023-07-05 14:54:57 +0800

    Some fixes

--- backend/src/mongodb/mongodb-connection.ts
+++ backend/src/mongodb/mongodb-connection.ts
@@ -38,27 +38,6 @@ export class MongoDBConnection {
       }
       await client.connect();
       console.log('MongoDB connection successful!');
-      //   // erase from here
-      //   const dbName = 'test';
-      //   const db = client.db(dbName);
-      //   // Use the collection "people"
-      // const col = db.collection('people');
-      // // Construct a document
-      // const personDocument = {
-      //   name: { first: 'Alan', last: 'Turing' },
-      //   birth: new Date(1912, 5, 23), // May 23, 1912
-      //   death: new Date(1954, 5, 7), // May 7, 1954
-      //   contribs: ['Turing machine', 'Turing test', 'Turingery'],
-      //   views: 1250000,
-      // };
-      // // Insert a single document, wait for promise so we can read it back
-      // const p = await col.insertOne(personDocument);
-      // // Find one document
-      // const myDoc = await col.findOne();
-      // // Print to the console
-      // console.log(myDoc);
-
-      // erase to here
     } catch (error) {
       console.error('[connect]', error);
     }
